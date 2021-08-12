9509ac0e5c0f9e4bf21a5c30539a4a5e9bd64916
ahmetdarendeli <ahmet.darendeli@vizyoneks.com.tr>
2021-08-13 02:31:26 +0300

    Project continue 3

--- app/src/main/java/com/example/socccer_league/MainActivity.java
+++ app/src/main/java/com/example/socccer_league/MainActivity.java
@@ -7,8 +7,10 @@ import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.ActivityOptions;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -69,6 +71,7 @@ public class MainActivity extends AppCompatActivity  implements View.OnClickList
         if (v == btnFixture) {
             Intent newIntent = new Intent(MainActivity.this,FixtureActivity.class);
             startActivity(newIntent);
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
         }
     }
 }
